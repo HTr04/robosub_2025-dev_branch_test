e8c2d1bc204667063af2df2e41386d126c3afab2
HTr04 <huytranlam123@gmail.com>
2025-06-25 09:58:20 -0700

    roll motion test

--- robosub_2025-dev_branch/tests/motion_tests/roll_test_depth_hold.py
+++ robosub_2025-dev_branch/tests/motion_tests/roll_test_depth_hold.py
@@ -1,24 +1,32 @@
 import rospy
 import time
+
 from auv.motion import robot_control
 from auv.utils import arm, disarm
 
 
-rospy.init_node("roll_test", anonymous=True)
-rc = robot_control.RobotControl(enable_dvl=False)
+def main():
+    rospy.init_node("roll_depth_hold_refined", anonymous=True)
+    rc = robot_control.RobotControl(enable_dvl=False)
 
-arm.arm()
-time.sleep(3.0)
+    arm.arm()
+    time.sleep(3.0)
+
+    rc.set_depth(0.7)
+    rc.set_mode("ALT_HOLD")
+    rospy.sleep(5.0)
+
+    rate = rospy.Rate(10)  # 10 Hz update rate
+    start_time = rospy.Time.now().to_sec()
+    while rospy.Time.now().to_sec() - start_time < 5:
+        rc.movement(roll=5)
+        rate.sleep()
+
+    rc.movement(roll=0)
+    rospy.sleep(1.0)
+
+    disarm.disarm()
 
 
-rc.set_depth(0.7)
-rc.set_mode("ALT_HOLD")
-time.sleep(5.0)
-
-first_time = time.time()
-while time.time() - first_time < 5:
-    rc.movement(roll=5)
-
-time.sleep(1.0)
-
-disarm.disarm()
+if __name__ == "__main__":
+    main()
