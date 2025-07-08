988eb8fae7985b62992cb28ad5dd6e79cd13687f
HTr04 <160572840+HTr04@users.noreply.github.com>
2025-07-08 07:49:48 -0700

    Update test_trained_model.py

--- robosub_2025-dev_branch/auv/yolomodel_train/test_trained_model.py
+++ robosub_2025-dev_branch/auv/yolomodel_train/test_trained_model.py
@@ -1,11 +1,14 @@
+"""This script tests a trained YOLOv8 model on a video file.
+It loads the model, runs object detection on the video,
+and saves the output video with detected objects highlighted."""
 from ultralytics import YOLO
 
 # Load the trained YOLOv8 model
-model = YOLO('C:/Users/HOME/Downloads/best_1.pt')  # Change to your actual model path
+model = YOLO('C:/Users/HOME/Downloads/best.pt')  # Change to your actual model path
 
 # Run detection on your video
 results = model.predict(
-    source='C:/Users/HOME/Documents/GitHub/CV_data/Image_video_test_4.mp4',    # Path to your video
+    source='C:/Users/HOME/Documents/GitHub/CV_data/poles_test_1.mp4',    # Path to your video
     save=True,             # Save the output video with boxes drawn
     conf=0.25,             # Confidence threshold (adjust as needed)
     show=True              # Show live results (works locally, not in Colab)
